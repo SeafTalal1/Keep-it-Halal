a0619a23d5f5f74ddabe07948d8ecb2d1874cf2b
Seaf Talal <seaftalal2017@gmail.com>
2024-12-24 07:04:44 +0200

    use clear screen function

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from functions import *
 
 if __name__ == "__main__":
     create_db()
+    clear_screen()
     while True:
         print("-" * 50)
         command = input("1. Add Domain\n2. Delete Domain\n3. View all Blocked IPs\n4. Quit\n> ")
@@ -10,15 +11,19 @@ if __name__ == "__main__":
         if command.isdigit():
             command = int(command)
             if command == 1:
+                clear_screen()
                 domain = input("Enter Domain to add : ")
                 ips = get_ips(domain)
                 save_blocked_ips_db(domain, ips)
             elif command == 2:
+                clear_screen()
                 domain = input("Enter Domain to delete : ")
                 delete_domain_from_db(domain)
             elif command == 3:
+                clear_screen()
                 view_blocked_ips()
             elif command == 4:
                 print("Program CLosed Succesfully")
                 time.sleep(1)
+                clear_screen()
                 break
